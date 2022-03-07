e44d0f882745db7262395e62dbbc16b5c1e77943
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2022-03-07 11:13:13 +0000

    update the style of the burguer menu

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let navBtn=document.querySelector(".nav-btn");
+let barIcon= document.querySelector(".fa-bars");
 const nav =document.querySelector("#nav")
 navBtn.addEventListener("click",function(){
+   barIcon.classList.toggle("second-color")
+   navBtn.classList.toggle("rotate")
    nav.classList.toggle("show-menu");
    console.log("hello");
 })
