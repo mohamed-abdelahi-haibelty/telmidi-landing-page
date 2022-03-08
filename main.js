5158f5694829f79b927c5b4633ea0384242777ed
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2022-03-08 16:20:28 +0000

    update burgure menu

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+//selectors
 let navBtn=document.querySelector(".nav-btn");
+let sideBar=document.querySelector(".side-bar");
+let closeBtn=document.querySelector(".close-btn");
 let barIcon= document.querySelector(".fa-bars");
-const nav =document.querySelector("#nav")
+
+//eventLisstener
 navBtn.addEventListener("click",function(){
-   barIcon.classList.toggle("second-color")
-   navBtn.classList.toggle("rotate")
-   nav.classList.toggle("show-menu");
+    barIcon.classList.add("second-color")
+    navBtn.classList.add("rotate")
+   sideBar.style.transform="translate(0)";
    console.log("hello");
+   closeBtn.classList.remove("rotate");
 })
+closeBtn.addEventListener("click",function(){
+   sideBar.style.transform="translate(-100%)";
+   closeBtn.classList.add("rotate");
+   barIcon.classList.remove("second-color")
+    navBtn.classList.remove("rotate")
+});
